b5f536f911b22f3003ed4c47ae2b68aa8ebf60e9
Sunny Aggarwal <sunnya97@protonmail.ch>
2020-09-15 21:09:45 -0400

    add to index

--- src/strategies/index.ts
+++ src/strategies/index.ts
@@ -3,11 +3,13 @@ import { strategy as erc20BalanceOf } from './erc20-balance-of';
 import { strategy as erc20BalanceOfFixedTotal } from './erc20-balance-of-fixed-total';
 import { strategy as erc20BalanceOfCv } from './erc20-balance-of-cv';
 import { strategy as makerDsChief } from './maker-ds-chief';
+import { strategy as erc721BalanceOf } from './erc721-balance-of';
 
 export default {
   balancer,
   erc20BalanceOf,
   erc20BalanceOfFixedTotal,
   erc20BalanceOfCv,
-  makerDsChief
+  makerDsChief,
+  erc721BalanceOf
 };
